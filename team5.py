bf1c6cce96bf3f5171a592f139ef28c74aff42b3
larsontheodore <larson_theodore@asd.lan>
2017-03-16 13:14:22 -0500

    Complete the test
    The tests are now passed

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Dy1an T3d ' # Only 10 chars displayed.
-strategy_name = 'Operation Swamp Vengence'
-strategy_description = 'Starts with collude, and then repeats the opponants last decision.'
+team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+strategy_name = 'The name the team gives to this strategy'
+strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -47,12 +47,12 @@ def test_move(my_history, their_history, my_score, their_score, result):
 
 if __name__ == '__main__':
      
-    # Test 1: Betray on the first move.
+    # Test 1: Betray on first move.
     if test_move(my_history='',
               their_history='', 
               my_score=0,
               their_score=0,
-              result='c'):
+              result='b'):
          print 'Test passed'
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
@@ -65,4 +65,4 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='c')             
+              result='b')             
